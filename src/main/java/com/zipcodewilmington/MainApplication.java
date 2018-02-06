6badb41e6b8cc6d6efe8d1038fc894d0e7f347ed
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-06 15:46:21 -0500

    first commit

---
+++ src/main/java/com/zipcodewilmington/MainApplication.java
@@ -0,0 +1,8 @@
+package com.zipcodewilmington;
+// command 1 - hides task pane
+// shift + fn + F10 - run automatically
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
